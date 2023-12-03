330b09500a420045c76010fb6b8c09c0ebaaae7a
Salemokey <flipish96@gmail.com>
2023-12-03 20:12:43 +0300

    доделать

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,28 @@
     crateMonitorLink = linkTovar ('monitor');
     createNotebookLink = linkTovar ('notebook');
     console.log (crateMonitorLink(4004));
+
+    function task2 () {
+        function skidka
+        return `Заказ покупателя ${orders.fio} составил ${orders.price*orders.quantity}`}
+        let orders= [
+            {
+                    fio:"Ivan",
+                    tovar:"phone",
+                    price:1000,
+                    quantity:2
+            },
+            {
+                fio:"Peter",
+                tovar:"usb",
+                price:2300,
+                quantity:5
+            },
+            {
+                fio:"Mary",
+                tovar:"connector",
+                price:100,
+                quantity:100
+            } ]
+
+        
